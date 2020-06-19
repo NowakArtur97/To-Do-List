4fd2d1763f0c9919c37c896890a007349101682a
NowakArtur97 <nowart97@gmail.com>
2020-06-19 17:57:41 +0200

    Drag and drop notes

--- js/dragAndDrop.js
+++ js/dragAndDrop.js
@@ -1,6 +1,7 @@
 export default class DragAndDrop {
   #element = null;
   #isDragged = false;
+  #offset = [0, 0];
 
   constructor(element) {
     this.#element = element;
@@ -9,10 +10,33 @@ export default class DragAndDrop {
   }
 
   #addEventListeners = function () {
-    this.#element.addEventListener("mousedown", this.#drag.bind(this));
+    this.#element.addEventListener("mousedown", this.#startDragging.bind(this));
+    this.#element.addEventListener("mousemove", this.#drag.bind(this));
+    this.#element.addEventListener("mouseup", this.#drop.bind(this));
+    this.#element.addEventListener("mouseleave", this.#drop.bind(this));
   };
 
-  #drag = function () {
-    console.log("DRAG");
+  #startDragging = function (e) {
+    this.#isDragged = true;
+    this.#offset = [
+      this.#element.offsetLeft - e.clientX,
+      this.#element.offsetTop - e.clientY,
+    ];
+  };
+
+  #drag = function (e) {
+    if (!this.#isDragged) {
+      return;
+    }
+    const mousePosition = {
+      x: e.clientX,
+      y: e.clientY,
+    };
+    this.#element.style.left = mousePosition.x + this.#offset[0] + "px";
+    this.#element.style.top = mousePosition.y + this.#offset[1] + "px";
+  };
+
+  #drop = function () {
+    this.#isDragged = false;
   };
 }
