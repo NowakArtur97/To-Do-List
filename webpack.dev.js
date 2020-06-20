156bdb46057208165dbfc6db89c501d0adb1c9f9
NowakArtur97 <nowart97@gmail.com>
2020-06-20 17:08:53 +0200

    Install and configure webpack dev server

--- webpack.dev.js
+++ webpack.dev.js
@@ -5,6 +5,10 @@ const merge = require("webpack-merge");
 module.exports = merge(common, {
   mode: "development",
   devtool: "none",
+  devServer: {
+    port: 8081,
+    host: "127.0.0.1",
+  },
   output: {
     filename: "app.js",
     path: path.resolve(__dirname, "dist"),
