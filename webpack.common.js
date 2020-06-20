b050ddaea710241f87c14629c59b69336fce16a1
NowakArtur97 <nowart97@gmail.com>
2020-06-20 16:53:40 +0200

    Cache app.js file and install Html Webpack Plugin

--- webpack.common.js
+++ webpack.common.js
@@ -1,11 +1,12 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
   devtool: "none",
   entry: "./src/index.js",
   output: {
-    filename: "app.js",
+    filename: "app-[contentHash].js",
     path: path.resolve(__dirname, "dist"),
   },
   module: {
@@ -16,4 +17,9 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/app/html/index.html",
+    }),
+  ],
 };
