51c6d898449326591c63651feed6c723ecf8bc1a
NowakArtur97 <nowart97@gmail.com>
2020-09-09 09:37:45 +0200

    Change methods and properties access to private in Note Cleaner

--- src/main/app/js/model/NoteCleaner.js
+++ src/main/app/js/model/NoteCleaner.js
@@ -1,21 +1,22 @@
-import DOMElements from "../dom/DOMElements";
-import ObserverManager from "../observer/ObserverManager";
+import DOMElements from '../dom/DOMElements';
+import ObserverManager from '../observer/ObserverManager';
 
 export default class NoteCleaner {
+  #events;
   constructor() {
-    this.events = new ObserverManager("deleteAll");
+    this.#events = new ObserverManager("deleteAll");
 
-    this.addEventListeners();
+    this.#addEventListeners();
   }
 
-  addEventListeners() {
+  #addEventListeners() {
     DOMElements.noteDeleteAllTrigger.addEventListener(
       "click",
-      this.deleteAll.bind(this)
+      this.#deleteAll.bind(this)
     );
   }
 
-  deleteAll() {
-    this.events.notify("deleteAll");
+  #deleteAll() {
+    this.#events.notify("deleteAll");
   }
 }
