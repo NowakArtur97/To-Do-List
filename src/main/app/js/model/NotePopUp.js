abe3a76ae4dcd34b38b887c74bb271fb03b2ca26
NowakArtur97 <nowart97@gmail.com>
2020-09-09 09:55:20 +0200

    Add private modifiers to methods and properties in Note Pop Up

--- src/main/app/js/model/NotePopUp.js
+++ src/main/app/js/model/NotePopUp.js
@@ -1,17 +1,17 @@
-import DOMElements from "../dom/DOMElements";
-import DOMClasses from "../dom/DOMClasses";
-import FormUtil from "../util/FormUtil";
+import DOMClasses from '../dom/DOMClasses';
+import DOMElements from '../dom/DOMElements';
+import FormUtil from '../util/FormUtil';
 
 export default class NotePopUp {
   constructor() {
-    this.addEventListeners();
+    this.#addEventListeners();
   }
 
-  addEventListeners() {
+  #addEventListeners() {
     DOMElements.noteFormPopUpTrigger.addEventListener("click", this.showPopUp);
     DOMElements.noteFormPopUpCloseBtn.addEventListener(
       "click",
-      this.closePopUp.bind(this)
+      this.#closePopUp.bind(this)
     );
   }
 
@@ -20,7 +20,7 @@ export default class NotePopUp {
     DOMElements.noteFormPopUp.classList.add(DOMClasses.noteFormPopUp.active);
   }
 
-  closePopUp() {
+  #closePopUp() {
     DOMElements.noteFormPopUp.classList.remove(DOMClasses.noteFormPopUp.active);
     FormUtil.resetForm(DOMElements.noteForm);
   }
