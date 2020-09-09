529f32809949dcc54a7c3f7d6500bb1c5b6d1a5a
NowakArtur97 <nowart97@gmail.com>
2020-09-09 10:03:56 +0200

    Add private modifiers to methods and properties in Note Filter Service

--- src/main/app/js/service/NoteFilterService.js
+++ src/main/app/js/service/NoteFilterService.js
@@ -1,14 +1,16 @@
 export default class NoteFilterService {
+  #taskService;
+  #noteService;
   constructor(taskService, noteService) {
-    this.taskService = taskService;
-    this.noteService = noteService;
+    this.#taskService = taskService;
+    this.#noteService = noteService;
   }
 
   filterTasks(value, property) {
     let tasks = value
-      ? this.taskService.filter(value, property)
-      : this.taskService.getAll();
-    this.noteService.deleteAll();
-    this.noteService.createAll(tasks);
+      ? this.#taskService.filter(value, property)
+      : this.#taskService.getAll();
+    this.#noteService.deleteAll();
+    this.#noteService.createAll(tasks);
   }
 }
